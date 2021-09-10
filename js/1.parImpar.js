0401b84d2b630b543770a855bad5577a9d780089
Javier F <javier.ferrer@iesmartinezm.es>
2021-09-10 11:18:02 +0200

    mi primer commit

---
+++ js/1.parImpar.js
@@ -0,0 +1,9 @@
+//Saber si un número es par o impar
+let numero=1;
+
+if (numero%2===0){
+    console.log(`El número ${numero} es par`);
+}
+else{
+    console.log(`El número ${numero} es impar`);
+}
