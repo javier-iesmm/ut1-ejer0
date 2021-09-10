8348881868387d722685985f18ac5bd619897302
Javier F <javier.ferrer@iesmartinezm.es>
2021-09-10 17:32:31 +0200

    mi primer commit en arrow-functions

--- js/1.parImpar.js
+++ js/1.parImpar.js
@@ -1,9 +1,12 @@
 //Saber si un número es par o impar
 let numero=1;
 
+const parImpar = function (numero){
 if (numero%2===0){
     console.log(`El número ${numero} es par`);
 }
 else{
     console.log(`El número ${numero} es impar`);
 }
+}
+parImpar;
